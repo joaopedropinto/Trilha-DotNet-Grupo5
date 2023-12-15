ea9dd82ac66bac0ef5b20484d8c5375618339b14
joaopedropinto <jpcic07@gmail.com>
2023-12-15 17:25:29 -0300

    adding class example

---
+++ ResTIConnect/ResTIConnect.Domain/Common/BaseEntity.cs
@@ -0,0 +1,10 @@
+namespace ResTIConnect.Domain.Common;
+public abstract class BaseEntity
+{
+  public Guid Id { get; set; }
+  public DateTimeOffset DateCreated { get; set; }
+
+  public DateTimeOffset DateUpdated { get; set; }
+
+  public DateTimeOffset DateDeleted { get; set; }
+}
