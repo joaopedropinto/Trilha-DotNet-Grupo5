57c9e844f7514bfe2f78d595262d39d5aa82804c
Daniel97 <dapsantos.cic@uesc.br>
2024-01-06 15:10:52 -0300

    Finishing P002

--- DOTNET-P002/Program.cs
+++ DOTNET-P002/Program.cs
@@ -13,30 +13,30 @@ namespace DOTNET_P002
             var joaoReport = Joao.View();
             var valberReport = Valber.View();
             var jhonataReport = Jhonata.View();
-            //var danielReport = Daniel.View();
+            var danielReport = Daniel.View();
             
             Console.WriteLine("Resumo das Habilidades da Equipe:\n");
             
             //Kayque Piton
             Console.WriteLine(kayqueReport);
             
-            //João Pedro de Gois Pinto")
+            //João Pedro de Gois Pinto
             Console.WriteLine(joaoReport);
             
-            //Valber Francisco dos Santos"
+            //Valber Francisco dos Santos
             Console.WriteLine(valberReport);
             
-            //Daniel Araújo"
+            //Jhonata Araújo
             Console.WriteLine(jhonataReport);
 
-            //Daniel Penedo"
-            //Console.WriteLine(danielReport);
+            //Daniel Penedo
+            Console.WriteLine(danielReport);
 
             var totalStars = Kayque.Skills.Sum(skill => skill.Item2) +
                              Joao.Skills.Sum(skill => skill.Item2) +
                              Valber.Skills.Sum(skill => skill.Item2) +
-                             Jhonata.Skills.Sum(skill => skill.Item2);
-                            // Daniel.Skills.Sum(skill => skill.Item2);
+                             Jhonata.Skills.Sum(skill => skill.Item2)+
+                             Daniel.Skills.Sum(skill => skill.Item2);
 
             Console.WriteLine($"\nTotal de estrelas da equipe: {totalStars}\n");
         }
