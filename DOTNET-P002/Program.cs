48737379716cf77627d43e340ab737858d9d5f84
Daniel97 <dapsantos.cic@uesc.br>
2024-01-06 15:07:21 -0300

    Modifying Program.cs

--- DOTNET-P002/Program.cs
+++ DOTNET-P002/Program.cs
@@ -1,11 +1,44 @@
-using DOTNET_P002.WebAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+namespace DOTNET_P002
+{
+    class Program
+    {
+        static void Main()
+        {
+            var kayqueReport = Kayque.View();
+            var joaoReport = Joao.View();
+            var valberReport = Valber.View();
+            var jhonataReport = Jhonata.View();
+            var danielReport = Daniel.View();
+            
+            Console.WriteLine("Resumo das Habilidades da Equipe:\n");
+            
+            //Kayque Piton
+            Console.WriteLine(kayqueReport);
+            
+            //João Pedro de Gois Pinto
+            Console.WriteLine(joaoReport);
+            
+            //Valber Francisco dos Santos
+            Console.WriteLine(valberReport);
+            
+            //Jhonata Araújo
+            Console.WriteLine(jhonataReport);
 
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/valber/", () => Valber.View());
-app.MapGet("/jhonata/", () => Jhonata.View());
-app.MapGet("/joao/", () => Joao.View());
+            //Daniel Penedo
+            Console.WriteLine(danielReport);
 
-app.Run();
+            var totalStars = Kayque.Skills.Sum(skill => skill.Item2) +
+                             Joao.Skills.Sum(skill => skill.Item2) +
+                             Valber.Skills.Sum(skill => skill.Item2) +
+                             Jhonata.Skills.Sum(skill => skill.Item2)+
+                             Daniel.Skills.Sum(skill => skill.Item2);
+
+            Console.WriteLine($"\nTotal de estrelas da equipe: {totalStars}\n");
+        }
+    }
+}
