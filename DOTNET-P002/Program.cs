d79d681ad2f602bbaa0b917c217924d405f20bc8
DStalkerBR <jhon89araujo@gmail.com>
2024-01-14 23:09:45 -0300

    Add view and routes for all members

--- DOTNET-P002/Program.cs
+++ DOTNET-P002/Program.cs
@@ -7,30 +7,35 @@ namespace DOTNET_P002
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
             var kayqueReport = Kayque.View();
             var joaoReport = Joao.View();
             var valberReport = Valber.View();
             var jhonataReport = Jhonata.View();
             var danielReport = Daniel.View();
-            
-            Console.WriteLine("Resumo das Habilidades da Equipe:\n");
-            
-            //Kayque Piton
-            Console.WriteLine(kayqueReport);
-            
-            //João Pedro de Gois Pinto
-            Console.WriteLine(joaoReport);
-            
-            //Valber Francisco dos Santos
-            Console.WriteLine(valberReport);
-            
-            //Jhonata Araújo
-            Console.WriteLine(jhonataReport);
 
-            //Daniel Penedo
-            Console.WriteLine(danielReport);
+            app.MapGet("/", () => View());
+            app.MapGet("/valber/", () => Valber.View());
+            app.MapGet("/jhonata/", () => Jhonata.View());
+            app.MapGet("/joao/", () => Joao.View());
+            app.MapGet("/kayque/", () => Kayque.View());
+            app.MapGet("/daniel/", () => Daniel.View());
+            app.Run();
+        }
+
+        public static string View()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("Resumo das Habilidades da Equipe:\n");
+            sb.AppendLine(Kayque.View());
+            sb.AppendLine(Joao.View());
+            sb.AppendLine(Valber.View());
+            sb.AppendLine(Jhonata.View());
+            sb.AppendLine(Daniel.View());
 
             var totalStars = Kayque.Skills.Sum(skill => skill.Item2) +
                              Joao.Skills.Sum(skill => skill.Item2) +
@@ -38,7 +43,9 @@ namespace DOTNET_P002
                              Jhonata.Skills.Sum(skill => skill.Item2)+
                              Daniel.Skills.Sum(skill => skill.Item2);
 
-            Console.WriteLine($"\nTotal de estrelas da equipe: {totalStars}\n");
+            sb.AppendLine($"\nTotal de estrelas da equipe: {totalStars}\n");
+
+            return sb.ToString();
         }
     }
 }
