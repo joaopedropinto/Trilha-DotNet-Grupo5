154c3c964636c28f3887011b2d6c58abd1fc93ae
joaopedropinto <jpcic07@gmail.com>
2024-01-21 17:21:48 -0300

    adding id_03, cliente and tecnico classes

--- Ordem_Servico.Infra/Context/OrdemServicoContext.cs
+++ Ordem_Servico.Infra/Context/OrdemServicoContext.cs
@@ -5,6 +5,8 @@ public class OrdemServicoContext : DbContext
 {
     public DbSet<OrdemServico> OrdemServico { get; set; }
     public DbSet<Finalizacao> Finalizacao { get; set; }
+    public DbSet<Cliente> Cliente { get; set; }
+    public DbSet<Tecnico> Tecnico { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         base.OnConfiguring(optionsBuilder);
@@ -19,10 +21,21 @@ public class OrdemServicoContext : DbContext
         base.OnModelCreating(modelBuilder);
         modelBuilder.Entity<Finalizacao>().ToTable("Finalizacao").HasKey(f => f.FinalizacaoID);
         modelBuilder.Entity<OrdemServico>().ToTable("OrdemServico").HasKey(o => o.OrdemServicoID);
+        modelBuilder.Entity<Cliente>().ToTable("Cliente").HasKey(c => c.ClienteID);
+        modelBuilder.Entity<Tecnico>().ToTable("Tecnico").HasKey(t => t.TecnicoID);
+
 
         modelBuilder.Entity<OrdemServico>()
             .HasOne(os => os.Finalizacao)
             .WithOne(f => f.OrdemServico)
-            .HasForeignKey<Finalizacao>(os => os.FinalizacaoID);
+            .HasForeignKey<Finalizacao>(os => os.FinalizacaoID)
+            .HasForeignKey<Cliente>(c => c.ClienteID)
+            .HasForeignKey<Tecnico>(t => t.TecnicoID);
+        
+        modelBuilder.Entity<Cliente>()
+            .HasMany(c => c.OrdemServico);
+
+        modelBuilder.Entity<Tecnico>()
+            .HasMany(c => c.OrdemServico);
     }
 }
