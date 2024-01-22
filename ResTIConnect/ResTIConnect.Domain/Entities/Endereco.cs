31d19094a7263af4cb3704e03c3e5f1763731a48
DStalkerBR <jhon89araujo@gmail.com>
2024-01-22 19:20:23 -0300

    Add Usuario entity and update related entities

--- ResTIConnect/ResTIConnect.Domain/Entities/Endereco.cs
+++ ResTIConnect/ResTIConnect.Domain/Entities/Endereco.cs
@@ -11,4 +11,6 @@ public class Endereco
    public string Estado{get;set;}
    public string Cep{get;set;}
    public string Pais{get;set;}
+   public int UsuarioId{get;set;}
+   public Usuario? Usuario{get;set;}
 }
