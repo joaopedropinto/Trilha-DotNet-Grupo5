8dfcb8160f3175d491758c30b7112618ecfbcf76
DStalkerBR <jhon89araujo@gmail.com>
2024-01-22 19:39:00 -0300

    Add DbSet for Usuario and configure relationships with other entities

--- ResTIConnect/ResTIConnect.Infra/Context/ResTIConnectContext.cs
+++ ResTIConnect/ResTIConnect.Infra/Context/ResTIConnectContext.cs
@@ -8,6 +8,7 @@ public class ResTIConnectContext : DbContext
     public DbSet<Log>? Logs { get; set; }
     public DbSet<Endereco>? Enderecos { get; set; }
     public DbSet<Perfil>? Perfis { get; set; }
+    public DbSet<Usuario>? Usuarios { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
@@ -20,6 +21,21 @@ public class ResTIConnectContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        base.OnModelCreating(modelBuilder);
 
+        modelBuilder.Entity<Log>().ToTable("Logs").HasKey(l => l.LogId);
+        modelBuilder.Entity<Endereco>().ToTable("Enderecos").HasKey(e => e.EnderecoId);
+        modelBuilder.Entity<Perfil>().ToTable("Perfis").HasKey(p => p.PerfilId);
+        modelBuilder.Entity<Usuario>().ToTable("Usuarios").HasKey(u => u.UsuarioId);
+
+        modelBuilder.Entity<Usuario>()
+            .HasOne(u => u.Endereco)
+            .WithOne(e => e.Usuario)
+            .HasForeignKey<Endereco>(e => e.EnderecoId);
+        
+        modelBuilder.Entity<Usuario>()
+            .HasMany(u => u.Perfis)
+            .WithOne(p => p.Usuario)
+            .HasForeignKey(p => p.UsuarioId);
     }
 }
