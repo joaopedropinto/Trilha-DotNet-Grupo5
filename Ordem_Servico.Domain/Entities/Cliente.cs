d433cc8cbef1b327af529284a9eb1c88b07aadf5
Kayque Piton <kayquepiton@gmail.com>
2024-02-06 20:56:53 -0300

    Atualizar o Cliente.cs
    Correção removendo o set

--- Ordem_Servico.Domain/Entities/Cliente.cs
+++ Ordem_Servico.Domain/Entities/Cliente.cs
@@ -1,13 +1,17 @@
-namespace Ordem_Servico.Domain;
-public class Cliente
+using System.Collections.Generic;
+
+namespace Ordem_Servico.Domain
 {
-    public int ClienteID { get; set; }
-    public string? CPF { get; set; }
-    public string? CNPJ { get; set; }
-    public string? Nome { get; set; }
-    public string? Telefone { get; set; }
-    public string? Email { get; set; }
-    public string? Endereco { get; set; }
-    public ICollection<OrdemServico>? OrdemServico {get; set;}
-    public int OrdemServicoID { get; set; }
+    public class Cliente
+    {
+        public int ClienteID { get; set; }
+        public string? CPF { get; set; }
+        public string? CNPJ { get; set; }
+        public string? Nome { get; set; }
+        public string? Telefone { get; set; }
+        public string? Email { get; set; }
+        public string? Endereco { get; set; }
+        public ICollection<OrdemServico>? OrdemServicos { get; }
+        public int OrdemServicoID { get; }
+    }
 }
