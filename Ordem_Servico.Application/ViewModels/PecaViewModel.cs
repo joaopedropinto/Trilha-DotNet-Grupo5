b7df443193079ded22f109e3776530a8c5e50443
DStalkerBR <jhon89araujo@gmail.com>
2024-02-11 18:39:43 -0300

    create peca models and service

---
+++ Ordem_Servico.Application/ViewModels/PecaViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Ordem_Servico.Application.ViewModels;
+
+public class PecaViewModel
+{
+    public int PecaID { get; set; }
+    public string? Tipo { get; set; }
+    public string? Descricao { get; set; }
+    public double Valor { get; set; }
+    public ICollection<int>? OrdemServicosID { get; set; }
+}
--- Ordem_Servico.Domain/Entities/Peca.cs
+++ Ordem_Servico.Domain/Entities/Peca.cs
@@ -4,7 +4,7 @@ public class Peca : BaseEntity
 {
     public int PecaID { get; set; }
     public string? Tipo { get; set; }
-    public string? Descrição { get; set; } // Corrigido de Descrição para Descrição
+    public string? Descricao { get; set; } 
     public double Valor { get; set; }
-    public ICollection<OrdemServico>? OrdemServicos { get; }
+    public ICollection<OrdemServico>? OrdemServicos { get; set; }
 }
