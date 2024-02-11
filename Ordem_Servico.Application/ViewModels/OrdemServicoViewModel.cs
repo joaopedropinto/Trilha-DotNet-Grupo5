cf9b8fd9491fe46e9dc0826e1df1d8658ce982aa
DStalkerBR <jhon89araujo@gmail.com>
2024-02-11 18:08:39 -0300

    update ordem servico models and services

--- Ordem_Servico.Application/ViewModels/OrdemServicoViewModel.cs
+++ Ordem_Servico.Application/ViewModels/OrdemServicoViewModel.cs
@@ -7,13 +7,13 @@ public class OrdemServicoViewModel
     public int OrdemServicoID { get; set; }
     public DateTime DataAbertura { get; set; }
     public DateTime Prazo { get; set; }
-    public required string? FormaPagamento { get; set; }
+    public string? FormaPagamento { get; set; }
     public required string? Status { get; set; }
     public int ClienteID { get; set; }
     public int TecnicoID { get; set; }
-    public required Finalizacao? Finalizacao { get; set; }
-    public required ICollection<int>? Equipamentos { get; set; }
-    public required ICollection<int>? Servicos { get; set; }
-    // public required ICollection<Ocorrencia> Ocorrencias { get; set; }
-    // public required ICollection<Peca> Pecas { get; set; }
+    public required FinalizacaoViewModel? Finalizacao { get; set; }
+    public required ICollection<int>? EquipamentosIDs { get; set; }
+    public required ICollection<int>? ServicosIDs { get; set; }
+    public required ICollection<int>? OcorrenciasIDs { get; set; }
+    public required ICollection<int>? PecasIDs { get; set; }
 }
