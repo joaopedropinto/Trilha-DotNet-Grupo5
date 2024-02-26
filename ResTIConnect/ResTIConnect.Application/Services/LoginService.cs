9bea06ca040c91235ef8601925c45eca04b8cf1a
kayquepiton <kayquepiton@gmail.com>
2024-02-26 18:38:08 -0300

    create login

--- ResTIConnect/ResTIConnect.Application/Services/LoginService.cs
+++ ResTIConnect/ResTIConnect.Application/Services/LoginService.cs
@@ -1,62 +1,59 @@
 using ResTIConnect.Application.Services.Interfaces;
 using ResTIConnect.Application.InputModels;
 using ResTIConnect.Application.ViewModels;
-using ResTIConnect.Domain.Entities;
 using ResTIConnect.Infra.Context;
-using ResTIConnect.Domain.Exceptions;
 using Microsoft.EntityFrameworkCore;
 
-namespace ResTIConnect.Application.Services
+namespace ResTIConnect.Application.Services;
+public class LoginService : ILoginService
 {
-    public class LoginService : ILoginService
+    private readonly ResTIConnectContext _dbcontext;
+
+    public LoginService(ResTIConnectContext dbcontext)
     {
-        private readonly ResTIConnectContext _dbcontext;
+        _dbcontext = dbcontext;
+    }
 
-        public LoginService(ResTIConnectContext dbcontext)
+    public async Task<UsuarioViewModel?> Authenticate(NewLoginInputModel login)
+    {
+        var _usuario = await _dbcontext.Usuarios
+            .Include(u => u.Endereco)
+            .Include(u => u.Perfis)
+            .FirstOrDefaultAsync(u => u.Email == login.Email && u.Senha == login.Senha);
+
+        if (_usuario == null)
         {
-            _dbcontext = dbcontext;
+            return null;
         }
 
-        public async Task<UsuarioViewModel?> Authenticate(NewLoginInputModel login)
+        return new UsuarioViewModel
         {
-            var _usuario = await _dbcontext.Usuarios
-                .Include(u => u.Endereco)
-                .Include(u => u.Perfis)
-                .FirstOrDefaultAsync(u => u.Email == login.Email && u.Senha == login.Senha);
-
-            if (_usuario == null)
+            UsuarioId = _usuario.UsuarioId,
+            Nome = _usuario.Nome,
+            Apelido = _usuario.Apelido,
+            Email = _usuario.Email,
+            Telefone = _usuario.Telefone,
+            Endereco = new EnderecoViewModel
             {
-                return null;
-            }
-
-            return new UsuarioViewModel
+                EnderecoId = _usuario.Endereco?.EnderecoId ?? 0,
+                Logradouro = _usuario.Endereco?.Logradouro,
+                Numero = _usuario.Endereco?.Numero ?? 0,
+                Cidade = _usuario.Endereco?.Cidade,
+                Complemento = _usuario.Endereco?.Complemento,
+                Bairro = _usuario.Endereco?.Bairro,
+                Estado = _usuario.Endereco?.Estado,
+                Cep = _usuario.Endereco?.Cep,
+                Pais = _usuario.Endereco?.Pais,
+                UsuarioId = _usuario.Endereco?.UsuarioId
+            },
+            Perfis = _usuario.Perfis?.Select(p => new PerfilViewModel
             {
-                UsuarioId = _usuario.UsuarioId,
-                Nome = _usuario.Nome,
-                Apelido = _usuario.Apelido,
-                Email = _usuario.Email,
-                Telefone = _usuario.Telefone,
-                Endereco = new EnderecoViewModel
-                {
-                    EnderecoId = _usuario.Endereco?.EnderecoId ?? 0,
-                    Logradouro = _usuario.Endereco?.Logradouro,
-                    Numero = _usuario.Endereco?.Numero ?? 0,
-                    Cidade = _usuario.Endereco?.Cidade,
-                    Complemento = _usuario.Endereco?.Complemento,
-                    Bairro = _usuario.Endereco?.Bairro,
-                    Estado = _usuario.Endereco?.Estado,
-                    Cep = _usuario.Endereco?.Cep,
-                    Pais = _usuario.Endereco?.Pais,
-                    UsuarioId = _usuario.Endereco?.UsuarioId
-                },
-                Perfis = _usuario.Perfis?.Select(p => new PerfilViewModel
-                {
-                    PerfilId = p.PerfilId,
-                    Descricao = p.Descricao,
-                    Permissoes = p.Permissoes,
-                    UsuarioId = p.UsuarioId
-                }).ToList()
-            };
-        }
+                PerfilId = p.PerfilId,
+                Descricao = p.Descricao,
+                Permissoes = p.Permissoes,
+                UsuarioId = p.UsuarioId
+            }).ToList()
+        };
     }
 }
+
